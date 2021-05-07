af4a8184b8392ff2712db46b1bc499e202c1e0c2
Shane Curtis <c84a@ymail.com>
2021-05-07 05:18:20 +0000

    Tidied up code formatting

--- index.js
+++ index.js
@@ -1,90 +1,78 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown')
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the name of your project?',
-     },
+  {
+    type: "input",
+    name: "title",
+    message: "What is the name of your project?",
+  },
 
+  {
+    type: "input",
+    name: "description",
+    message: "Please enter a description of the project",
+  },
 
-     {
-        type: 'input',
-        name: 'description',
-        message: 'Please enter a description of the project',
-     },
+  {
+    type: "input",
+    name: "install",
+    message: "What are the steps required to install your project?",
+  },
 
-     {
-        type: 'input',
-        name: 'install',
-        message: 'What are the steps required to install your project?'
-      },
+  {
+    type: "input",
+    name: "usage",
+    message: "Provide instructions on how to use the application.",
+  },
 
-      {
-         type: 'input',
-         name: 'usage',
-         message: 'Provide instructions on how to use the application.'
-      },
+  {
+    type: "list",
+    name: "license",
+    message: "Which license would you like to use?",
+    choices: ["Apache", "GNU", "MIT", "ISC", "None"],
+  },
 
-      {
-         type:'list',
-         name:'license',
-         message:'Which license would you like to use?',
-         choices: [ 
-         'Apache',
-         'GNU',
-         'MIT',
-         'ISC',
-         'None',
-      ]
-      },
+  {
+    type: "input",
+    name: "credits",
+    message:
+      "List any collaborators with links to their GitHub profiles. List any third party assets or tutorials used.",
+  },
 
-      {
-         type: 'input',
-         name: 'credits',
-         message:'List any collaborators with links to their GitHub profiles. List any third party assets or tutorials used.' 
-      },
-     
-      {
-         type: 'input',
-         name: 'testing',
-         message:'Provide any tests that have been created for your application and how to run them.' 
-      },
+  {
+    type: "input",
+    name: "testing",
+    message:
+      "Provide any tests that have been created for your application and how to run them.",
+  },
 
-   
-
-      {
-      type: 'input',
-      name: 'github',
-      message: 'Please enter your Github username',
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: 'Please enter your email address',
-    },
-   
-     ]
-     
-
+  {
+    type: "input",
+    name: "github",
+    message: "Please enter your Github username",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Please enter your email address",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(filename, markdown) {
-   fs.writeFile(filename, markdown, (err) =>
-     err ? console.log(err) : console.log("Success!")
-   );
- }
- 
- function init() {
-     inquirer.prompt(questions)
-     .then((data) => {
-         const markdown = generateMarkdown(data);
-         writeToFile('readme.md', markdown);
-       });
- }
- 
+  fs.writeFile(filename, markdown, (err) =>
+    err ? console.log(err) : console.log("Success!")
+  );
+}
 
- init();
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    const markdown = generateMarkdown(data);
+    writeToFile("readme.md", markdown);
+  });
+}
+
+init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,36 +1,26 @@
-
-
-
-
-
-
-
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   switch (license) {
-    case 'Apache':
-    return 'Apache-2.0 (https://opensource.org/licenses/Apache-2.0)'
-    break
-    case 'GNU':
-      return 'GNU GLPv3 (https://choosealicense.com/licenses/gpl-3.0/)'
+    case "Apache":
+      return "Apache-2.0 (https://opensource.org/licenses/Apache-2.0)";
       break;
-    case 'MIT':  
-    return 'MIT (https://choosealicense.com/licenses/mit/)'
-    break;
-   
-    case 'ISC':
-    return 'ISC (https://choosealicense.com/licenses/isc/)'
-    break;
-    default: 'Not currently licensed'
-      return ""
+    case "GNU":
+      return "GNU GLPv3 (https://choosealicense.com/licenses/gpl-3.0/)";
+      break;
+    case "MIT":
+      return "MIT (https://choosealicense.com/licenses/mit/)";
+      break;
+
+    case "ISC":
+      return "ISC (https://choosealicense.com/licenses/isc/)";
+      break;
+    default:
+      "Not currently licensed";
+      return "";
       break;
   }
-
-
 }
-
-
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -77,7 +67,9 @@ function generateMarkdown(data) {
 
   ## Questions
 
-  If you have any questions about this project, please feel free to contact me at ${data.email} or to see more of my work https://github.com/${data.github}/
+  If you have any questions about this project, please feel free to contact me at ${
+    data.email
+  } or to see more of my work https://github.com/${data.github}/
   
 
 `;
